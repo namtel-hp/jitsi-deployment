f52888d80a9570e3bd5446167d56cf86c39d582b
namtel <62697590+namtel-hp@users.noreply.github.com>
2020-11-16 01:13:54 +0700

    Update cluster.tf

--- cluster/dev/cluster.tf
+++ cluster/dev/cluster.tf
@@ -1,6 +1,6 @@
 resource "profitbricks_k8s_cluster" "dev" {
   name        = "jitsi-dev"
-  k8s_version = "1.17.13"
+  k8s_version = "1.17.13-do.2"
   maintenance_window {
     day_of_the_week = "Saturday"
     time            = "03:49:18Z"
@@ -9,7 +9,7 @@ resource "profitbricks_k8s_cluster" "dev" {
 
 resource "profitbricks_k8s_node_pool" "dev_zone_1" {
   name              = "worker-3cpu-8gb"
-  k8s_version       = "1.17.13"
+  k8s_version       = "1.17.13-do.2"
   maintenance_window {
     day_of_the_week = "Saturday"
     time            = "16:09:33Z"
@@ -27,7 +27,7 @@ resource "profitbricks_k8s_node_pool" "dev_zone_1" {
 
 resource "profitbricks_k8s_node_pool" "dev_zone_2" {
   name              = "worker-3cpu-8gb"
-  k8s_version       = "1.17.13"
+  k8s_version       = "1.17.13-do.2"
   maintenance_window {
     day_of_the_week = "Wednesday"
     time            = "23:30:49Z"
@@ -35,7 +35,7 @@ resource "profitbricks_k8s_node_pool" "dev_zone_2" {
   datacenter_id     = var.datacenter
   k8s_cluster_id    = profitbricks_k8s_cluster.dev.id
   cpu_family        = "INTEL_XEON"
-  availability_zone = "SGP1"
+  availability_zone = "FRA1"
   storage_type      = "HDD"
   node_count        = 2
   cores_count       = 3
