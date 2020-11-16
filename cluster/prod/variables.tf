66c061b50937e77d1f5fccb6f865bee6bb09eb05
namtel <62697590+namtel-hp@users.noreply.github.com>
2020-11-16 09:50:21 +0700

    Update variables.tf

--- cluster/prod/variables.tf
+++ cluster/prod/variables.tf
@@ -1,5 +1,5 @@
 variable "datacenter" {
   description = "Video Data Center"
   type        = string
-  default     = "73785743c88a01d990d4f69e1713151aa827ccdc9dded247648f75a7a6424e84"
+  default     = "df8bd51c-298f-4981-8df8-6fd07340b397"
 }
